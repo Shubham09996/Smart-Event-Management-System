148b78ed9571ebd352820e9be59fad1608e6652b
Shubham Gupta <shubham0996@gmail.com>
2025-08-27 00:43:45 +0530

    added vercel config

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const connectDB = async () => {
 connectDB();
 
 app.use(cors({
-  origin: 'http://localhost:5174',
+  origin: ['http://localhost:5174', 'https://smart-event-management-system.vercel.app'],
   credentials: true,
 }));
 app.use(express.json({ limit: '50mb' })); // Increased limit for JSON body
